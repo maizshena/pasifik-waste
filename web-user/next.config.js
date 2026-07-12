51e75d156dbb34983068cdbdd1589a098d4fafc3
@maizshena <shainaabinaya@gmail.com>
2026-07-12 09:46:47 +0700

    fix: next.config.js structure

--- web-user/next.config.js
+++ web-user/next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint:     { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname:  'localhost',
-        port:      '4000',
-        pathname:  '/uploads/**',
-          typescript: { ignoreBuildErrors: true },
-  eslint:     { ignoreDuringBuilds: true },
-      },
+      { protocol: 'http',  hostname: 'localhost', port: '4000', pathname: '/uploads/**' },
+      { protocol: 'https', hostname: '*.railway.app',           pathname: '/uploads/**' },
     ],
   },
 };
